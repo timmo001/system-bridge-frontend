b4cb55db9bac19737e2222971e8df4fe07b36e2e
Aidan Timson <aidan@timmo.dev>
2023-11-26 17:08:51 +0000

    Output

--- components/Data/DataItems.tsx
+++ components/Data/DataItems.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useMemo } from "react";
 import { CardContent, CircularProgress, Grid, Typography } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 
 import { type GridInitialStateCommunity } from "@mui/x-data-grid/models/gridStateCommunity";
 import { type Modules } from "types/models";
@@ -26,6 +26,8 @@ const defaultInitialState: GridInitialStateCommunity = {
   },
 };
 
+const defaultPageSizeOptions: number[] = [10, 20, 40, 50, 100, 200];
+
 export default function DataItemsComponent({
   title,
   data,
@@ -33,12 +35,32 @@ export default function DataItemsComponent({
   title: string;
   data?: Modules;
 }): ReactElement {
-  const rootData = useMemo(() => {
+  const dataRoot = useMemo<GridRowsProp>(() => {
     if (!data) return [];
     const dataGridData = [];
     for (const [key, value] of Object.entries(data)) {
       if (Array.isArray(value) || typeof value === "object") continue;
       dataGridData.push({ id: key, key, value });
+    }
+    return dataGridData;
+  }, [data]);
+
+  const dataArrays = useMemo<Array<Record<string, GridRowsProp>>>(() => {
+    if (!data) return [];
+    const dataGridData = [];
+    for (const [key, value] of Object.entries(data)) {
+      if (!value || !Array.isArray(value)) continue;
+      const dataGridDataArray = [];
+      for (const [innerKey, innerValue] of Object.entries(value)) {
+        if (Array.isArray(innerValue) || typeof innerValue === "object")
+          continue;
+        dataGridDataArray.push({
+          id: innerKey,
+          key: innerKey,
+          value: innerValue,
+        });
+      }
+      dataGridData.push({ [key]: dataGridDataArray });
     }
     return dataGridData;
   }, [data]);
@@ -49,12 +71,35 @@ export default function DataItemsComponent({
         {title}
       </Typography>
       {data ? (
-        <DataGrid
-          columns={defaultColumns}
-          initialState={defaultInitialState}
-          pageSizeOptions={[10, 20, 40, 50, 100, 200]}
-          rows={rootData}
-        />
+        <>
+          {dataRoot.length > 0 && (
+            <DataGrid
+              autoHeight
+              columns={defaultColumns}
+              initialState={defaultInitialState}
+              pageSizeOptions={defaultPageSizeOptions}
+              rows={dataRoot}
+            />
+          )}
+          {dataArrays.map((dataArray) => {
+            const key = Object.keys(dataArray)[0];
+            const value = dataArray[key];
+            return (
+              <>
+                <Typography gutterBottom variant="h5" component="h4">
+                  {key}
+                </Typography>
+                <DataGrid
+                  autoHeight
+                  columns={defaultColumns}
+                  initialState={defaultInitialState}
+                  pageSizeOptions={defaultPageSizeOptions}
+                  rows={value}
+                />
+              </>
+            );
+          })}
+        </>
       ) : (
         <Grid container direction="row" justifyContent="center">
           <CircularProgress />
