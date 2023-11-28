0ce530d85bfc8cb506a7a0e5435055811df0b0a9
Aidan Timson <aidan@timmo.dev>
2023-11-28 18:41:48 +0000

    Restore JSON view

--- components/Data/DataItems.tsx
+++ components/Data/DataItems.tsx
@@ -1,37 +1,12 @@
-import { ReactElement, useMemo } from "react";
-import { CardContent, CircularProgress, Grid, Typography } from "@mui/material";
-import {
-  DataGrid,
-  GridColDef,
-  GridRowsProp,
-  GridToolbar,
-} from "@mui/x-data-grid";
+import { ReactElement } from "react";
+import { CircularProgress, Grid, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
 
-import { type GridInitialStateCommunity } from "@mui/x-data-grid/models/gridStateCommunity";
 import { type Modules } from "types/models";
 
-const defaultColumns: GridColDef[] = [
-  { field: "key", headerName: "Name", width: 240 },
-  { field: "value", headerName: "Value", width: 480 },
-];
-
-const defaultInitialState: GridInitialStateCommunity = {
-  sorting: {
-    sortModel: [
-      {
-        field: "key",
-        sort: "asc",
-      },
-    ],
-  },
-  pagination: {
-    paginationModel: {
-      pageSize: 40,
-    },
-  },
-};
-
-const defaultPageSizeOptions: number[] = [10, 20, 40, 50, 100, 200];
+const BrowserReactJsonView = dynamic(() => import("react-json-view"), {
+  ssr: false,
+});
 
 export default function DataItemsComponent({
   title,
@@ -40,63 +15,6 @@ export default function DataItemsComponent({
   title: string;
   data?: Modules;
 }): ReactElement {
-  const dataRoot = useMemo<GridRowsProp>(() => {
-    if (!data) return [];
-    const dataGridData = [];
-    for (const [key, value] of Object.entries(data)) {
-      if (!value ? false : Array.isArray(value) || typeof value === "object")
-        continue;
-      dataGridData.push({ id: key, key, value: String(value) });
-    }
-    return dataGridData;
-  }, [data]);
-
-  const dataArrays = useMemo<Array<Record<string, GridRowsProp>>>(() => {
-    if (!data) return [];
-    const dataGridData = [];
-    for (const [key, value] of Object.entries(data)) {
-      if (!value || !Array.isArray(value)) continue;
-      const dataGridDataArray = [];
-      for (const [innerKey, innerValue] of Object.entries(value)) {
-        if (Array.isArray(innerValue) || typeof innerValue === "object")
-          continue;
-        dataGridDataArray.push({
-          id: innerKey,
-          key: innerKey,
-          value: String(innerValue),
-        });
-      }
-      dataGridData.push({ [key]: dataGridDataArray });
-    }
-    return dataGridData;
-  }, [data]);
-
-  const dataObjects = useMemo<Array<Record<string, GridRowsProp>>>(() => {
-    if (!data) return [];
-    const dataGridData = [];
-    for (const [key, value] of Object.entries(data)) {
-      if (!value || Array.isArray(value) || typeof value !== "object") continue;
-      const dataGridDataArray = [];
-      for (const [innerKey, innerValue] of Object.entries(value)) {
-        if (
-          !innerValue
-            ? false
-            : Array.isArray(innerValue) || typeof innerValue === "object"
-        )
-          continue;
-        dataGridDataArray.push({
-          id: innerKey,
-          key: innerKey,
-          value: String(innerValue),
-        });
-      }
-      dataGridData.push({ [key]: dataGridDataArray });
-    }
-    return dataGridData;
-  }, [data]);
-
-  console.log({ dataRoot, dataArrays, dataObjects });
-
   return (
     <>
       <Typography gutterBottom variant="h4" component="h3">
@@ -104,60 +22,17 @@ export default function DataItemsComponent({
       </Typography>
       {data ? (
         <>
-          {dataRoot.length > 0 && (
-            <DataGrid
-              autoHeight
-              columns={defaultColumns}
-              disableRowSelectionOnClick
-              initialState={defaultInitialState}
-              pageSizeOptions={defaultPageSizeOptions}
-              rows={dataRoot}
-              slots={{ toolbar: GridToolbar }}
-              sx={{ mb: 2 }}
-            />
-          )}
-          {dataArrays.map((dataArray) => {
-            const key = Object.keys(dataArray)[0];
-            const value = dataArray[key];
-            return (
-              <section key={key}>
-                <Typography gutterBottom variant="h5" component="h4">
-                  {key}
-                </Typography>
-                <DataGrid
-                  autoHeight
-                  columns={defaultColumns}
-                  disableRowSelectionOnClick
-                  initialState={defaultInitialState}
-                  pageSizeOptions={defaultPageSizeOptions}
-                  rows={value}
-                  slots={{ toolbar: GridToolbar }}
-                  sx={{ mb: 2 }}
-                />
-              </section>
-            );
-          })}
-          {dataObjects.map((dataObject: Record<string, GridRowsProp>) => {
-            const key = Object.keys(dataObject)[0];
-            const value = dataObject[key];
-            return (
-              <section key={key}>
-                <Typography gutterBottom variant="h5" component="h4">
-                  {key}
-                </Typography>
-                <DataGrid
-                  autoHeight
-                  columns={defaultColumns}
-                  disableRowSelectionOnClick
-                  initialState={defaultInitialState}
-                  pageSizeOptions={defaultPageSizeOptions}
-                  rows={value}
-                  slots={{ toolbar: GridToolbar }}
-                  sx={{ mb: 2 }}
-                />
-              </section>
-            );
-          })}
+          <BrowserReactJsonView
+            src={data}
+            displayDataTypes={false}
+            displayObjectSize
+            enableClipboard
+            iconStyle="triangle"
+            name={null}
+            collapseStringsAfterLength={140}
+            style={{ background: "initial", maxWidth: "100%" }}
+            theme="google"
+          />
         </>
       ) : (
         <Grid container direction="row" justifyContent="center">
