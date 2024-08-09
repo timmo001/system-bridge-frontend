433262d73eff01c4d6f3fbdb0343772131527993
Aidan Timson <contact@timmo.xyz>
2024-08-09 11:23:08 +0100

    Update build file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
   output: "export",
   reactStrictMode: true,
+  eslint: {
+    // ESLint is disabled during builds to avoid slowing down the build process.
+    ignoreDuringBuilds: true,
+  },
+
 };
 
-module.exports = nextConfig;
+export default config;
